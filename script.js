f4d14d83be81bd4e257370866bf3172a58036e23
Matt Thompson <matthewthompson118@gmail.com>
2023-08-21 13:19:30 -0300

    Added new code

--- script.js
+++ script.js
@@ -17,6 +17,23 @@ function generatePassword()
 
   //Confirms user slelected a number between min and max
   if (length < 8 || length > 128) {
+    alert("Incorrect choice. Password must be between and 128 characters");
+    location.reload()
+
+
+} else {  // Variables and conditionals statements that log user choices and choose the criteria that applies to generated password}
+  var includeUpper = confirm("Include uppercase letters?");
+  var includeLower = confirm("Include lower case letters?");
+  var includeNUmber = confirm("Include Numerical characters?");
+  var includeSpecial = confirm("Include special characters?");
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var number = "0123456789";
+  var special = "~`!@#$%^&*()-_=+[{]}\|;:',<.>/?";
+  var charChoice = "";
 }
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

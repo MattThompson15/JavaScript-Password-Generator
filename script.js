cdebd5c7f8508eba12cd8000e45221df9a652106
Matt Thompson <matthewthompson118@gmail.com>
2023-08-21 17:56:27 -0300

    Added more code

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function writePassword() {
 function generatePassword() 
 
   //Prompt to enter desired length of password
-  var length=Number(prompt("How many characters will your password be?Please choose between 8 and 28 characters"));
+  var length=Number(prompt("How many characters will your password be? Please choose between 8 and 28 characters"));
 
   //Confirms user slelected a number between min and max
   if (length < 8 || length > 128) {
@@ -32,7 +32,63 @@ function generatePassword()
   var special = "~`!@#$%^&*()-_=+[{]}\|;:',<.>/?";
   var charChoice = "";
 }
+// If user chose to include at least one or all of the criteria.
 
+if (includeUpper) {
+  charChoice += upper;
+}
+if (inlcudeLower) {
+  charChoice += lower;
+}
+if (includeNumber) {
+  charChoice += number;
+}
+if (inlcudeSpecial) {
+  charChoice += special;
+}
+
+// If user did not follow any othe criteria selections, they must start over and choose at least one.
+if (charChoice == "") {
+  alert("Passwrod must inlcude at least one character from criteria selection, press OK to refresh the page")
+  location.reload();
+}
+
+do {
+  // Variable and statements that will generate new password based on user criteria choice.
+  var newPassword = "";
+  for (var i = 0; i < length; i++) {
+    //chooses random characters within the selected criteria category based on lenght set by user
+    newPassword += charChoice. charAt(Math.floor(Math.random() * charChoice.length));
+
+  }
+console.log(newPassword)
+var hasLower = false;
+var hasUpper = false;
+var hasNumber = false;
+var hasSpecial = false;
+
+for (var i = 0; i < newPassword.length; i++) {
+  var char = newPassword[i];
+
+  if (lower.includes(char)) {
+    hasLower = true;
+  } else if (upper.includes(char)) {
+    hasUpper = true;
+  } else if (number.includes(char)) {
+  } else if (special.includes(char)) {
+    hasSpecial = true;
+  }
+}
+
+} while (
+  (includeLower && !hasLower) ||
+  (includeUpper && !hasUpper) ||
+  (includeNUmer && !hasNUmber) ||
+  (includeSpecial && !hasSpecial) 
+)
+
+// return newPassword;
+return newPassword;
 
 
 // Add event listener to generate button
